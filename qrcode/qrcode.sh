9d037843c1c2427404e7d1bdb754de571aa6f07f
Nathan Higley <contact@nathanhigley.com>
2021-10-30 12:19:23 -0400

    Fixed qrcode missing url

---
+++ qrcode/qrcode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo
+echo "Join the LU Cyber Defense Club Discord!" | ccze -A
+echo
+echo
+
+qrcode-terminal {{ QR_CODE_URL }}
+
+echo
+echo "Meetings every Thursday night at 5PM" | ccze -A
+echo
+echo
